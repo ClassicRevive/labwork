973801f3163d9ad0dcebc6f97a646e0acf443e70
root <root@LAPTOP-HSLRDMV0.localdomain>
2019-12-05 13:39:44 +0000

    changed plot_point.py

--- lab16/plot-points.py
+++ lab16/plot-points.py
@@ -10,7 +10,7 @@ point_input = sys.stdin.readlines()
 i = 0
 while i < len(point_input):
     point = point_input[i].rstrip()
-    # add point to dictio
+    # add points to dictionary
     points[point] = True
 
     i += 1
@@ -21,29 +21,32 @@ first_line = " " + ("-" * 20) + " "
 
 # insert plotting code
 
-y = 19
-x = 0
+def generate_grid():
+    y = 19
+    x = 0
+
+    while y > -1:
+        # line = "|"
+        grid = ["|"]
+        x = 0
+        while x < 20:
+
+            coord = str(x) + " " + str(y)
+            if coord in points:
+                grid.append("*")
+            else:
+                grid.append(" ")
+
+            x += 1
+        # line += "|"
+        # print line
+        grid.append("|")
+        new_grid = "".join(grid)
+        print new_grid
+        y -= 1
 
 print " " + "-" * 20
 
-while y > -1:
-    # line = "|"
-    grid = ["|"]
-    x = 0
-    while x < 20:
-
-        coord = str(x) + " " + str(y)
-        if coord in points:
-            grid.append("*")
-        else:
-            grid.append(" ")
-
-        x += 1
-    # line += "|"
-    # print line
-    grid.append("|")
-    new_grid = "".join(grid)
-    print new_grid
-    y -= 1
+generate_grid()
 
 print " " + "-" * 20
