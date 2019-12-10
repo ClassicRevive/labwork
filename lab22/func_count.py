2854e50474e17256c9c112b320fb0335c103fccb
Joseph Oluwasanya <joluwasanya@gmail.com>
2019-12-10 19:58:13 +0000

    completed lab 22 and added lab 20

--- lab22/func_count.py
+++ lab22/func_count.py
@@ -3,11 +3,13 @@
 import func_bsearch
 
 def count(a, q):
-    search = func_bsearch.bsearch(a, q)
-    if search == len(a) or a[search] != q:
-        return 0
-    else:
-        return a.count(q)
+    # find q
+    # find where next number isnt q
+    search1 = func_bsearch.bsearch(a, q)
+    search2 = func_bsearch.bsearch(a, q + 1)
+    count = search2 - search1
+
+    return count
 
 if __name__ == "__main__":
     test_list = [2, 4, 4, 7, 10, 10, 11, 20, 25, 25, 25, 25, 26]
